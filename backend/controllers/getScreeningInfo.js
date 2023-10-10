39ae758448a7d17b1554376863a79bf713792db0
Lucas Magnusson <lucasmyeet@gmail.com>
2023-10-10 14:57:17 +0200

    endpoint successfully gathers all screenings depending on movieid

--- backend/controllers/getScreeningInfo.js
+++ backend/controllers/getScreeningInfo.js
@@ -1,7 +1,13 @@
-import { screeninginfoService } from '../service/screeninginfoService.js'
+import { screeninginfoService } from '../service/screeninginfoService.js';
 
 export async function getScreeningInfo(req, res) {
     const { movieid } = req.params;
-    const result = await screeninginfoService(movieid)
-    res.send(result);
+    const result = await screeninginfoService(movieid);
+    if (!result || result.length === 0) {
+        // If no results were found, return a 404 Not Found response.
+        return res.status(404).send("Movie not found");
+    }
+
+    // If results were found, return a 200 OK response with the data.
+    res.status(200).json(result);
 }
