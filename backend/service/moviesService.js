ceba16d6e33367fa612b79350e9af14aa81fecc2
Kettler-gamer <filip.blomqvist.fkb@hotmail.com>
2023-10-11 10:18:56 +0200

    added function for getting movies by date from db

--- backend/service/moviesService.js
+++ backend/service/moviesService.js
@@ -18,4 +18,36 @@ async function getMovies() {
   return res;
 }
 
-export default { getMovies };
+async function getMoviesByDate(date) {
+  const sql = `SELECT 
+  title, 
+  CONVERT(image USING utf8mb4) AS image, 
+  durationinminutes, 
+  agelimit, 
+  GROUP_CONCAT(category) AS categories 
+  FROM movies 
+  INNER JOIN moviecategories 
+  INNER JOIN screenings 
+  LEFT JOIN categories 
+  ON moviecategories.categoryid = categories.id AND moviecategories.movieid = movies.id WHERE 
+  screenings.date > ? AND screenings.date < ? AND screenings.movieid = movies.id 
+  GROUP BY movies.id;`;
+
+  const result = await runQuery(sql, getDateQueryArray(date));
+
+  return result;
+}
+
+function getDateQueryArray(date) {
+  const startDate = new Date(date);
+  const nextDayTimeIncrease = startDate.getTime() + 24 * 60 * 60 * 1000;
+  const nextDayDate = new Date(date);
+  nextDayDate.setTime(nextDayTimeIncrease);
+
+  const startDateString = startDate.toLocaleDateString();
+  const nextDayString = nextDayDate.toLocaleDateString();
+
+  return [startDateString, nextDayString];
+}
+
+export default { getMovies, getMoviesByDate };
