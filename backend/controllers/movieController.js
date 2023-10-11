a1c153c2d349c1d32a6362f6f6acf7a13137a5a6
Kettler-gamer <filip.blomqvist.fkb@hotmail.com>
2023-10-11 10:18:20 +0200

    getting movies by date if specified

--- backend/controllers/movieController.js
+++ backend/controllers/movieController.js
@@ -1,6 +1,15 @@
 import moviesService from "../service/MoviesService.js";
 
 export async function getMovies(req, res) {
-  const result = await moviesService.getMovies();
+  const { date } = req.query;
+
+  let result;
+
+  if (date) {
+    result = await moviesService.getMoviesByDate(date);
+  } else {
+    result = await moviesService.getMovies();
+  }
+
   res.send(result);
 }
