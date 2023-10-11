6b05a133992a05795c82fb26d6be8c71bf25b289
j-carlander <johannes.carlander@gmail.com>
2023-10-11 10:10:42 +0200

    added comments

--- backend/utils/mailtransporter.js
+++ backend/utils/mailtransporter.js
@@ -1,3 +1,20 @@
+/**
+ * Util that sets up the nodemailer
+ * Specifies the details needed for the connection
+ * exports the transporter
+ *
+ * to use: import mailTransporter where needed
+ * to send mail: mailTransporter.sendMail(message, callback(err,info)*)
+ *   *callback is optional, if omitted a Promise is returned instead
+ *
+ * message format: {
+ * from: "filmvisarna.grupp1@gmail.com",
+ * to: guestemail/useremail,
+ * subject: "eg. Booking details",
+ * text: "Plain text version of message",
+ * html: "<p>HTML version of message</p>"}
+ */
+
 import nodemailer from "nodemailer";
 
 const details = {
