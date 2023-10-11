f3b9650d6fcbc538663b8823989f04e5bd0b5437
emelie8 <emelie_johansen@hotmail.com>
2023-10-11 08:43:56 +0200

    set up book routing routes

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -3,6 +3,8 @@ import { getTakenseats } from "../controllers/getTakenseats.js";
 import { getScreeningInfo } from "../controllers/getScreeningInfo.js";
 import { checkScreeningId } from "../middleware/checkScreeningId.js";
 import { getMovies } from "../controllers/movieController.js";
+import { checkBookingDetails } from "../middleware/checkBooking.js";
+import { addBooking } from "../controllers/book.js";
 
 const router = express.Router();
 
@@ -11,4 +13,5 @@ router.get("/takenseats/:screeningid", checkScreeningId, getTakenseats);
 router.get("/moviescreenings/:movieid", getScreeningInfo)
 
 router.get("/movies", getMovies);
+router.post("/booking/:screeningid", checkScreeningId, checkBookingDetails, addBooking);
 export default router;
