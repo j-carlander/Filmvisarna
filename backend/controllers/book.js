af055268275fb90c610cafe48663d8338d4047f6
Kettler-gamer <filip.blomqvist.fkb@hotmail.com>
2023-10-13 10:23:51 +0200

    added function for getting bookings

--- backend/controllers/book.js
+++ backend/controllers/book.js
@@ -1,42 +1,56 @@
-import { bookingNumberService } from "../service/bookingNumberService.js"
+import { bookingNumberService } from "../service/bookingNumberService.js";
 import { bookingTickets } from "../service/bookingTicketsService.js";
-import { bookingservice } from "../service/bookingservice.js";
+import {
+  bookingservice,
+  getBookingsByUserId,
+} from "../service/bookingservice.js";
 
-export async function addBooking (req, res) {
-    const {seats, guestEmail, guestPhone} = req.body;
-    const screeningid = req.params.screeningid
-    const bookingNumber = await bookingNumberService();
-    const bookResult = await bookingservice(bookingNumber, screeningid, guestEmail, guestPhone)
-    console.log(bookResult.insertId)
+export async function addBooking(req, res) {
+  const { seats, guestEmail, guestPhone } = req.body;
+  const screeningid = req.params.screeningid;
+  const bookingNumber = await bookingNumberService();
+  const bookResult = await bookingservice(
+    bookingNumber,
+    screeningid,
+    guestEmail,
+    guestPhone
+  );
+  console.log(bookResult.insertId);
 
-    const ticketresult = await seats.forEach(async seat => {
-        await bookingTickets (
-            seat.seatRow,
-            seat.seatNumber,
-            1,
-            screeningid,
-            bookResult.insertId,
-        )
-    });
-    
-    console.log(ticketresult)
+  const ticketresult = await seats.forEach(async (seat) => {
+    await bookingTickets(
+      seat.seatRow,
+      seat.seatNumber,
+      1,
+      screeningid,
+      bookResult.insertId
+    );
+  });
 
-    // bookingTickets(
-    //     seatrow,
-    //     seatnumber,
-    //     tickettypeid,
-    //     screeningid,
-    //     bookingid
-    // )
+  console.log(ticketresult);
 
-    const bookingDetails = {
-        seats: seats,
-        guestEmail: guestEmail,
-        guestPhone: guestPhone,
-        bookingNumber: bookingNumber,
-    };
+  // bookingTickets(
+  //     seatrow,
+  //     seatnumber,
+  //     tickettypeid,
+  //     screeningid,
+  //     bookingid
+  // )
 
-    
+  const bookingDetails = {
+    seats: seats,
+    guestEmail: guestEmail,
+    guestPhone: guestPhone,
+    bookingNumber: bookingNumber,
+  };
 
-    res.status(201).json(bookingDetails);
+  res.status(201).json(bookingDetails);
+}
+
+export async function getBookings(req, res) {
+  const { jwtPayload } = res.locals;
+
+  const bookings = await getBookingsByUserId(jwtPayload.id);
+
+  res.send(bookings);
 }
