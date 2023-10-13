bd66e2537ea4d4782647c6219711d2afad1d5e6f
j-carlander <johannes.carlander@gmail.com>
2023-10-13 16:14:27 +0200

    hotfixes in checkToken, book, getUser
    Co-authored-by: Filip <filip.blomqvist.fkb@hotmail.com>

--- backend/controllers/book.js
+++ backend/controllers/book.js
@@ -11,55 +11,52 @@ export async function addBooking(req, res) {
   const screeningid = req.params.screeningid;
   const bookingNumber = await bookingNumberService();
 
-  const userid = req.user ? req.user.id : null;
+  const userid = res.locals.jwtPayload ? res.locals.jwtPayload.id : null;
 
-  const ticketPromises = seats.map(async (seat) => {
-    await bookingTickets (
-      seat.seatRow, 
-      seat.seatNumber, 
-      1, 
-      screeningid,
-      bookResult.insertId);
-  });
-
-  await Promise.all(ticketPromises);
   const ticketType = await tickettypeService();
   const bookResult = await bookingservice(
     bookingNumber,
     screeningid,
     guestEmail,
     guestPhone,
-    userid,
+    userid
   );
   console.log(bookResult.insertId);
 
-  const ticketresult = await seats.map(async (seat) => {
-    await bookingTickets(
+  const ticketPromises = seats.map((seat) =>
+    bookingTickets(
       seat.seatRow,
       seat.seatNumber,
       ticketType.find(({ id }) => id === seat.ticketType).id,
       screeningid,
       bookResult.insertId
-    );
-  });
+    )
+  );
 
-  console.log(ticketresult);
+  await Promise.all(ticketPromises);
 
-  const bookingDetails = {
+  let bookingDetails = {
     seats: seats,
-    guestEmail: guestEmail,
-    guestPhone: guestPhone,
     bookingNumber: bookingNumber,
-    userid: userid,
   };
+
+  if (guestEmail) {
+    bookingDetails = {
+      ...bookingDetails,
+      guestEmail: guestEmail,
+      guestPhone: guestPhone,
+    };
+  }
 
   res.status(201).json(bookingDetails);
 }
 
 export async function getBookings(req, res) {
-  const { jwtPayload } = res.locals;
+  const payload = res.locals.jwtPayload;
 
-  const bookings = await getBookingsByUserId(jwtPayload.id);
+  if (!payload) return res.status(400).send("Token not provided!");
+
+  const bookings = await getBookingsByUserId(payload.id);
 
   res.send(bookings);
 }
