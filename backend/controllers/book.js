d17ee56adc90b39db769205566f39d85b5700eb7
Lucas Magnusson <lucasmyeet@gmail.com>
2023-10-13 14:52:41 +0200

    hotfix

--- backend/controllers/book.js
+++ backend/controllers/book.js
@@ -7,23 +7,24 @@ export async function addBooking(req, res) {
   const screeningid = req.params.screeningid;
   const bookingNumber = await bookingNumberService();
 
-  // Check if a user is logged in and get their user ID from req.user
   const userid = req.user ? req.user.id : null;
 
-  // Create the booking in the database
-  const bookResult = await bookingservice(bookingNumber, screeningid, guestEmail, guestPhone, userid);
+  const bookResult = await bookingservice (
+    bookingNumber, 
+    screeningid, 
+    guestEmail, 
+    guestPhone, 
+    userid);
 
-  // Insert tickets for the booking
   const ticketPromises = seats.map(async (seat) => {
     await bookingTickets (
-        seat.seatRow, 
-        seat.seatNumber, 
-        1, 
-        screeningid,
-        bookResult.insertId);
+      seat.seatRow, 
+      seat.seatNumber, 
+      1, 
+      screeningid,
+      bookResult.insertId);
   });
 
-  // Wait for all ticket insertions to complete
   await Promise.all(ticketPromises);
 
   const bookingDetails = {
@@ -31,6 +32,7 @@ export async function addBooking(req, res) {
     guestEmail: guestEmail,
     guestPhone: guestPhone,
     bookingNumber: bookingNumber,
+    userid: userid,
   };
 
   res.status(201).json(bookingDetails);
