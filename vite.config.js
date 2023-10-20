0f94e20aa7ba2ae918edd5c4d92e41c460020a01
emelie8 <emelie_johansen@hotmail.com>
2023-10-20 15:11:05 +0200

    basic frontend setup
    Co-authored-by: Filip <filip.blomqvist.fkb@hotmail.com>
    Co-authored-by: Daniella <daniella.karlen@gmail.com>
    Co-authored-by: Mohamed <mohamadalihassan99@outlook.com>
    Co-authored-by: Lucas <lucasbjornerikmagnusson@hotmail.com>
    Co-authored-by: Johannes <johannes.carlander@gmail.com>

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
+import { reactEasierViteConfig as revc_ } from 'react-easier/vite-config'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(revc_({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000/',
+        changeOrigin: true,
+        secure: false
+      }
+    }
+  }
+}))
