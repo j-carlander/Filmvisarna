50e5926f7bbe19885c62038cf923b4e9be7dc738
j-carlander <johannes.carlander@gmail.com>
2023-10-24 14:35:04 +0200

    restore app.jsx after testing component

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,8 @@
 import "./App.css";
 import { Outlet } from "react-router-dom";
-import { ScreeningDetails } from "./components/ScreeningDetails/ScreeningDetails";
 
-const testscreening = {
-  id: 45,
-  screeningDate: "Tisdag 24 okt. kl: 19:00",
-  language: "sv",
-  subtitle: "sv",
-  theatre: "Lilla Visaren",
-  totalSeats: "55",
-  freeSeats: "55",
-};
 function App() {
-  return (
-    <>
-      <ScreeningDetails screening={testscreening} />
-      <Outlet />
-    </>
-  );
+  return <Outlet />;
 }
 
 export default App;
