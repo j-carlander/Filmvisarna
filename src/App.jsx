5b83e544f4418cf08e25985a4d131ef2d6b21e49
MohamadAliHassan <113009645+MohamadAliHassan@users.noreply.github.com>
2023-10-26 21:24:11 +0200

    worked on styling bookingpage

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { Outlet } from "react-router-dom";
+// import { Outlet } from "react-router-dom";
 import BookingPage from "./pages/Bookingpage/BookingPage";
 
 function App() {
--- src/pages/Bookingpage/BookingPage.jsx
+++ src/pages/Bookingpage/BookingPage.jsx
@@ -1,8 +1,5 @@
-// TODO: Import användarens prefered movie från filmdetaljsidan
-//       Se till att du har states som hanterar priserna för ordinarie, pensionärer
-
 import React, { useState } from 'react';
-// import Seat from '../../components/Seats/Seat/Seat';
+import Seat from '../../components/Seats/Seat/Seat';
 
 function BookingPage() {
   const [ordinariePris, setOrdinariePris] = useState(140);
@@ -16,39 +13,52 @@ function BookingPage() {
   const totalPris =
     (ordinariePris * ordinarieAntal) + (pensionärPris * pensionärAntal) + (barnPris * barnAntal);
 
+    if(barnAntal < 0){
+      setBarnAntal(0)
+    }
+
+    if(pensionärAntal < 0){
+      setPensionärAntal(0)
+    }
+
+    if(ordinarieAntal < 0){
+      setOrdinarieAntal(0)
+    }
   return (
     <>
       <div className="Bookingpage-container">
-        <h3>Barbie 1t:54m - <p>7+</p></h3>
-        <h3>Lördag kl 20:00 - <p>Stora Visaren, Slut ca kl 22:00</p></h3>
+        <div className="header-content">
+          <h3>Barbie 1t:54m - <p>7+</p></h3>
+          <h3>Lördag kl 20:00 - <p>Stora Visaren, Slut ca kl 22:00</p></h3>
+        </div>
         
-        <h1>Välj antal biljetter</h1>
         <div className="ticket-wrapper">
-          <p>Ordinarie ({ordinariePris}kr)</p>
+          <h1>Välj antal biljetter</h1>
           <div className="btn-container">
+            <p>Ordinarie ({ordinariePris}kr)</p>
             <button onClick={() => setOrdinarieAntal(ordinarieAntal + 1)}>+</button>
             <p>{ordinarieAntal}</p>
             <button onClick={() => setOrdinarieAntal(ordinarieAntal - 1)}>-</button>
           </div>
-          <p>Pensionär ({pensionärPris}kr)</p>
           <div className="btn-container">
+          <p>Pensionär ({pensionärPris}kr)</p>
             <button onClick={() => setPensionärAntal(pensionärAntal + 1)}>+</button>
             <p>{pensionärAntal}</p>
             <button onClick={() => setPensionärAntal(pensionärAntal - 1)}>-</button>
           </div>
-          <p>Barn ({barnPris}kr)</p>
           <div className="btn-container">
+            <p>Barn ({barnPris}kr)</p>
             <button onClick={() => setBarnAntal(barnAntal + 1)}>+</button>
             <p>{barnAntal}</p>
             <button onClick={() => setBarnAntal(barnAntal - 1)}>-</button>
           </div>
+          <p className='sum'>Totalt kostnad: {totalPris} kr</p>
         </div>
-        <p>Totalt pris: {totalPris} kr</p>
 
         {/* Importera seats */}
         <div className="seat-section">
           <h2>Välj platser</h2>
-          {/* <Seat /> */}
+          <Seat />
           <p>Vald plats: rad 1 plats 1-2</p>
           <button>Boka biljett</button>
         </div>
