0fb588b7ae0906ded26d2d3c2a1d859f902c2a2a
MohamadAliHassan <113009645+MohamadAliHassan@users.noreply.github.com>
2023-10-27 10:19:22 +0200

    changes in bookingpage

--- src/pages/Bookingpage/BookingPage.jsx
+++ src/pages/Bookingpage/BookingPage.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import Seat from '../../components/Seats/Seat/Seat';
+import Seats from '../../components/Seats/Seats';
 
 function BookingPage() {
   const [ordinariePris, setOrdinariePris] = useState(140);
@@ -28,37 +28,15 @@ function BookingPage() {
     <>
       <div className="Bookingpage-container">
         <div className="header-content">
-          <h3>Barbie 1t:54m - <p>7+</p></h3>
+          <h3>Midnight in Paris 1t:54m - <p>7+</p></h3>
           <h3>Lördag kl 20:00 - <p>Stora Visaren, Slut ca kl 22:00</p></h3>
         </div>
         
-        <div className="ticket-wrapper">
-          <h1>Välj antal biljetter</h1>
-          <div className="btn-container">
-            <p>Ordinarie ({ordinariePris}kr)</p>
-            <button onClick={() => setOrdinarieAntal(ordinarieAntal + 1)}>+</button>
-            <p>{ordinarieAntal}</p>
-            <button onClick={() => setOrdinarieAntal(ordinarieAntal - 1)}>-</button>
-          </div>
-          <div className="btn-container">
-          <p>Pensionär ({pensionärPris}kr)</p>
-            <button onClick={() => setPensionärAntal(pensionärAntal + 1)}>+</button>
-            <p>{pensionärAntal}</p>
-            <button onClick={() => setPensionärAntal(pensionärAntal - 1)}>-</button>
-          </div>
-          <div className="btn-container">
-            <p>Barn ({barnPris}kr)</p>
-            <button onClick={() => setBarnAntal(barnAntal + 1)}>+</button>
-            <p>{barnAntal}</p>
-            <button onClick={() => setBarnAntal(barnAntal - 1)}>-</button>
-          </div>
-          <p className='sum'>Totalt kostnad: {totalPris} kr</p>
-        </div>
 
         {/* Importera seats */}
         <div className="seat-section">
           <h2>Välj platser</h2>
-          <Seat />
+          <Seats />
           <p>Vald plats: rad 1 plats 1-2</p>
           <button>Boka biljett</button>
         </div>
