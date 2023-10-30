1482778627ff7654bfd5e0fcdfb8819bdbf9fbf7
Lucas Magnusson <lucasmyeet@gmail.com>
2023-10-30 11:51:50 +0100

    hotfix import

--- src/components/Seats/Seats.jsx
+++ src/components/Seats/Seats.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import Seat from "./Seatrow/Seat/Seat";
+import Seat from "./Seat/Seat";
 import SeatRow from "./Seatrow/SeatRow";
 import { fetchHelper } from "../../utils/fetchHelper";
 
