ce14becf02efd22c2b7bcb4fce22f8ccc9bc3f99
Kettler-gamer <filip.blomqvist.fkb@hotmail.com>
2023-11-09 13:43:27 +0100

    added util for checking if seats are grouped

--- backend/utils/formatSeatsInfoForEmail.js
+++ backend/utils/formatSeatsInfoForEmail.js
@@ -1,10 +1,12 @@
+import { checkSeatsGroupedTogether } from "./checkSeatsGroupedTogether.js";
 import { checkSeatsOnSameRow } from "./checkSeatsOnSameRow.js";
 
 export function formatSeatInfo(seats) {
   if (seats.length === 0) return "";
   const onSameRow = checkSeatsOnSameRow(seats);
+  const groupedTogether = checkSeatsGroupedTogether(seats);
 
-  if (onSameRow) {
+  if (onSameRow && groupedTogether) {
     const seatNumbers =
       seats.length === 1
         ? seats[0].seatNumber
