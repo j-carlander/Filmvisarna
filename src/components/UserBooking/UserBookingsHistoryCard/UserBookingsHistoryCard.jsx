f2c30b6f61830b3dc67d8e740ae19290ded934c5
j-carlander <johannes.carlander@gmail.com>
2023-11-09 13:11:19 +0100

    styling of moviehistory on my account page

--- src/components/UserBooking/UserBookingsHistoryCard/UserBookingsHistoryCard.jsx
+++ src/components/UserBooking/UserBookingsHistoryCard/UserBookingsHistoryCard.jsx
@@ -1,20 +1,17 @@
-
 export function UserBookingsHistoryCard({ bookingData }) {
-    
-      
-        const dateArr = bookingData.date.split(". kl: ");
-      
-        const time = dateArr[1];
-      
-        const day = dateArr[0];
-      
-        return (
-          <li className="user-booking">
-            <div className="booking-info-hist">
-              <p>{bookingData.title},</p>
-              <p>{time},</p> 
-              <p>{day}</p>
-            </div>
-          </li>
-        );
+  const dateArr = bookingData.date.split(". kl: ");
+
+  const time = dateArr[1];
+
+  const day = dateArr[0];
+
+  return (
+    <li className="user-history-li">
+      <div className="booking-info-hist">
+        <p>{bookingData.title},</p>
+        <p>{time},</p>
+        <p>{day}</p>
+      </div>
+    </li>
+  );
 }
