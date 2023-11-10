1e7da80b012e2c87f9c7ef5bd22dfb4b4164170d
j-carlander <johannes.carlander@gmail.com>
2023-11-10 11:53:19 +0100

    admin page

--- src/pages/AdminPage/AdminPage.jsx
+++ src/pages/AdminPage/AdminPage.jsx
@@ -1,8 +1,10 @@
+import { useState } from "react";
+import { adminPages } from "../../main";
 import sessionService from "../../utils/sessionService";
-import { NavLink, Navigate } from "react-router-dom";
-import { AdminSearchBookingPage } from "../AdminSearchBookingPage/AdminSearchBookingPage";
+import { NavLink, Navigate, Outlet } from "react-router-dom";
 
 export function AdminPage() {
+  const [showMenu, setShowMenu] = useState(false);
   const token = sessionService.getToken();
   let role = "user";
   if (token) {
@@ -16,10 +18,51 @@ export function AdminPage() {
   return (
     <>
       <header className="admin-page-header">
-        <NavLink to={"/"}>Till hemsidan</NavLink>
         <h1 className="admin-page-title">Filmvisarna - Administrationssida</h1>
       </header>
-      <AdminSearchBookingPage />
+      <div className="admin-page-wrapper">
+        <aside className="admin-page-aside">
+          <button
+            className="admin-page-show-mobile-menu"
+            onClick={() => setShowMenu(!showMenu)}>
+            {showMenu ? (
+              <img src="/close.svg" alt="close menu" />
+            ) : (
+              <img src="/hamburger_menu.svg" alt="open menu" />
+            )}
+          </button>
+          <nav
+            className={
+              showMenu ? "admin-page-nav-open " : "admin-page-nav-close"
+            }>
+            <ul className="admin-page-nav-list">
+              <li>
+                {" "}
+                <NavLink to={"/"} end className={"admin-navlink"}>
+                  Till start
+                </NavLink>
+              </li>
+              {adminPages.map((page) => {
+                if (!("label" in page)) return null;
+                return (
+                  <li key={page.label}>
+                    <NavLink
+                      to={page.path}
+                      className={(isActive) =>
+                        isActive ? "active-admin-navlink" : "admin-navlink"
+                      }>
+                      {page.label}
+                    </NavLink>
+                  </li>
+                );
+              })}
+            </ul>
+          </nav>
+        </aside>
+        <main className="admin-page-main">
+          <Outlet />
+        </main>
+      </div>
     </>
   );
 }
