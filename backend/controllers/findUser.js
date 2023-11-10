1be62c0c96e5d5ea8f03cb61f6108d5b36976b8b
j-carlander <johannes.carlander@gmail.com>
2023-11-10 15:24:02 +0100

    change in findUser to search on name and mail by one query

--- backend/controllers/findUser.js
+++ backend/controllers/findUser.js
@@ -1,23 +1,18 @@
-import { getUserWithName, getUserWithMail, getUserWithBoth } from "../service/getUserAdminService.js"
+import { getUserWithBoth } from "../service/getUserAdminService.js";
 
 export async function findUser(req, res) {
-    try {
-        const { fname, lname, email } = req.query;
-        if (fname && lname && email) {
-            const users = await getUserWithBoth(fname, lname, email)
-            return res.json(users);
-        } else if (fname && lname) {
-            const users = await getUserWithName(fname, lname);
-            return res.json(users);
-        } else if (email) {
-            const user = await getUserWithMail(email);
-            return res.json(user)
-        } else {
-            return res.status(400).json({ error: "Please provide either first name and last name or email."})
-        }
-    } catch (error) {
-        console.error(error);
-        return res.status(500).json({ error: "Internal server error" })
+  try {
+    const { q } = req.query;
+    if (q) {
+      const users = await getUserWithBoth(q);
+      return res.json(users);
+    } else {
+      return res
+        .status(400)
+        .json({ error: "Du måste söka på ett namn eller en mailadress" });
     }
-
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: "Internal server error" });
+  }
 }
