c80ad69ca7d17bffbdf62a7d33f832dae33c68f4
Kettler-gamer <filip.blomqvist.fkb@hotmail.com>
2023-11-13 14:55:24 +0100

    set correct function name in import

--- backend/middleware/checkAddMovie.js
+++ backend/middleware/checkAddMovie.js
@@ -1,21 +1,30 @@
 // middleware/validateAddMovie.js
 export function validateAddMovie(req, res, next) {
-    const {
-      title,
-      description,
-      releaseDate,
-      trailerLink,
-      durationInMinutes,
-      ageLimit,
-      directorId,
-      categoryIds,
-      actorNames,
-    } = req.body;
-  
-    // Check if all required fields are present
-    if (!title || !description || !releaseDate || !trailerLink || !durationInMinutes || !ageLimit || !directorId || !categoryIds || !actorNames) {
-      return res.status(400).json({ error: "Alla fält är nödvändiga." });
-    }
-    next();
+  const {
+    title,
+    description,
+    releaseDate,
+    trailerLink,
+    durationInMinutes,
+    ageLimit,
+    directorId,
+    categoryIds,
+    actorNames,
+  } = req.body;
+
+  // Check if all required fields are present
+  if (
+    !title ||
+    !description ||
+    !releaseDate ||
+    !trailerLink ||
+    !durationInMinutes ||
+    !ageLimit ||
+    !directorId ||
+    !categoryIds ||
+    !actorNames
+  ) {
+    return res.status(400).json({ error: "Alla fält är nödvändiga." });
   }
-  
+  next();
+}
