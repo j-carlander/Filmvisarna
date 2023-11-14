a8f972f1646561e3fbce3d48e9c0a0b597f25130
j-carlander <johannes.carlander@gmail.com>
2023-11-14 22:33:35 +0100

    Service functions to send email to all bookings of a screening

--- backend/service/deleteBookingService.js
+++ backend/service/deleteBookingService.js
@@ -1,4 +1,5 @@
 import { runQuery } from "../db.js";
+import { removeScreeningMailService } from "./removeScreeningMailService.js";
 
 export async function deleteBookingGuestService(bookingnumber, guestemail) {
   const sql = `DELETE from bookings WHERE bookingnumber = ? AND guestemail = ? LIMIT 1`;
@@ -16,4 +17,23 @@ export async function getSeatsForDeletionService(bookingnumber) {
   const sql = `SELECT seatrow, seatnumber, screeningid FROM tickets WHERE bookingid = (select id from bookings where bookingnumber = ?);`;
   const res = await runQuery(sql, [bookingnumber]);
   return res;
+}
+
+// runs before screening is removed to update users that the screening is canceled
+export async function emailAllBookingsByScreeningService(screeningId) {
+  const selectDateTitleFromScreening =
+    "SELECT date, m.title FROM screenings AS s JOIN movies AS m ON s.movieid = m.id WHERE s.id = ?";
+
+  const res = await runQuery(selectDateTitleFromScreening, [screeningId]);
+  const { date, title } = res[0];
+
+  const selectEmailFromBookingsByScreeningId =
+    " SELECT CONCAT(COALESCE(b.guestemail,''), COALESCE(u.email,'')) AS email FROM bookings AS b LEFT JOIN users AS u ON b.userid = u.id WHERE b.screeningid = ?";
+  const emailsList = await runQuery(selectEmailFromBookingsByScreeningId, [
+    screeningId,
+  ]);
+  if (emailsList.length > 0) {
+    emailsList.map((email) => removeScreeningMailService(email, title, date));
+  }
+  return;
 }
