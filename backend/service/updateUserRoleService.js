cc1f8dd4e5270c521862f1b1ddd0c3bcf624f0de
MohamadAliHassan <113009645+MohamadAliHassan@users.noreply.github.com>
2023-11-21 08:42:10 +0100

    added comments in service file that starts with M-U

--- backend/service/updateUserRoleService.js
+++ backend/service/updateUserRoleService.js
@@ -1,21 +1,25 @@
+/*
+These JavaScript functions modify user roles in a database. The setUserRoleToAdmin function updates the role of a user with a specified ID to "admin," while the setUserRoleToUser function sets the role to "user." Both functions use SQL UPDATE queries and the runQuery function to execute the database updates, returning the result.
+*/
+
 import { runQuery } from "../db.js";
 
 export async function setUserRoleToAdmin(uid) {
-    const sql = `
+  const sql = `
     UPDATE users
     SET role = "admin"
     WHERE id = ?
-    `
-    const res = await runQuery(sql, uid)
-    return res;
+    `;
+  const res = await runQuery(sql, uid);
+  return res;
 }
 
 export async function setUserRoleToUser(uid) {
-    const sql = `
+  const sql = `
     UPDATE users
     SET role = "user"
     WHERE id = ?
-    `
-    const res = await runQuery(sql, uid)
-    return res;
+    `;
+  const res = await runQuery(sql, uid);
+  return res;
 }
