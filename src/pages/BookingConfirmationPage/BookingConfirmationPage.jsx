0e0380226885a7d9d7f3b0f5dc5482a81d5e4dfc
Kettler-gamer <filip.blomqvist.fkb@hotmail.com>
2023-11-21 09:06:44 +0100

    fixed crash if the user refreshes the page or navigates directly to the page

--- src/pages/BookingConfirmationPage/BookingConfirmationPage.jsx
+++ src/pages/BookingConfirmationPage/BookingConfirmationPage.jsx
@@ -1,5 +1,5 @@
-import { useLocation, useParams } from "react-router-dom";
-import { useState } from "react";
+import { useLocation, useParams, useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 import BookingConfirmation from "../../components/BookingConfirmation/BookingConfirmation";
 
@@ -8,57 +8,77 @@ import { formatSeatsForBookingConf } from "../../utils/formatSeatsForBookingConf
 import { ConfirmBookingCTA } from "../../components/ConfirmBookingCTA/ConfirmBookingCTA";
 
 export function BookingConfirmationPage() {
+  const navigate = useNavigate();
   const [confirmationData, setConfirmationData] = useState();
+  const [seats, setSeats] = useState([]);
+  const [screeningData, setScreeningData] = useState();
+  const [individual, setIndividual] = useState();
 
   const windowWidth = useWindowInnerWidth();
 
   const location = useLocation();
-  const { selectedSeats, selectedTickets, data, individual } = location.state;
-
   const { screeningId } = useParams();
 
-  const screeningData = {
-    id: data.movieid,
-    title: data.title,
-    date: data.screeningDate,
-    price: selectedTickets.reduce((acc, current) => current.price + acc, 0),
-    seats: formatSeatsForBookingConf(individual, selectedSeats),
-  };
+  useEffect(() => {
+    if (location.state === null) {
+      return navigate(`/bokning/${screeningId}`);
+    }
 
-  const seats = [];
-  for (let i = 0; i < selectedSeats.length; i++) {
-    seats.push({
-      ...selectedSeats[i],
-      seatRow: selectedSeats[i].rowNumber,
-      ticketType: selectedTickets[i].id,
-    });
-  }
+    const { selectedSeats, selectedTickets, data, individual } = location.state;
+    setIndividual(individual);
+    const newScreeningData = {
+      id: data.movieid,
+      title: data.title,
+      date: data.screeningDate,
+      price: selectedTickets.reduce((acc, current) => current.price + acc, 0),
+      seats: formatSeatsForBookingConf(individual, selectedSeats),
+    };
+    setScreeningData(newScreeningData);
+    const newSeats = [];
+    for (let i = 0; i < selectedSeats.length; i++) {
+      newSeats.push({
+        ...selectedSeats[i],
+        seatRow: selectedSeats[i].rowNumber,
+        ticketType: selectedTickets[i].id,
+      });
+    }
+    setSeats(newSeats);
+  }, [
+    location,
+    navigate,
+    screeningId,
+    setSeats,
+    setScreeningData,
+    setIndividual,
+  ]);
 
   return (
     <div className="bookconfirm-wrapper">
       <h1>Bokningsöversikt</h1>
       <div className="bookconfirm-page">
-        <div className="bookconfirm-screenings">
-          {windowWidth >= 850 && (
-            <img
-              className="bookconfirm-img"
-              src={`/images/${screeningData.id}_w400.webp`}
-              alt="movie-poster"
-            />
-          )}
-          <p>
-            <strong>Titel:</strong> {screeningData.title}
-          </p>
-          <p>
-            <strong>Tid:</strong> {screeningData.date}
-          </p>
-          <p className={individual ? "individual-seats" : ""}>
-            <strong>Valda Platser:</strong> {screeningData.seats}
-          </p>
-          <p>
-            <strong>Att Betala:</strong> {screeningData.price} kr
-          </p>
-        </div>
+        {screeningData !== undefined && (
+          <div className="bookconfirm-screenings">
+            {windowWidth >= 850 && (
+              <img
+                className="bookconfirm-img"
+                src={`/images/${screeningData.id}_w400.webp`}
+                alt="movie-poster"
+              />
+            )}
+            <p>
+              <strong>Titel:</strong> {screeningData.title}
+            </p>
+            <p>
+              <strong>Tid:</strong> {screeningData.date}
+            </p>
+            <p className={individual ? "individual-seats" : ""}>
+              <strong>Valda Platser:</strong> {screeningData.seats}
+            </p>
+            <p>
+              <strong>Att Betala:</strong> {screeningData.price} kr
+            </p>
+          </div>
+        )}
         <ConfirmBookingCTA {...{ seats, screeningId, setConfirmationData }} />
       </div>
       {confirmationData ? (
